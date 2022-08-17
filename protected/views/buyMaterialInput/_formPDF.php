2d0e3d060358a269a2a2ea3347e3dc69952c6f99
boybe <boybe7oo@gmail.com>
2022-08-18 02:54:46 +0700

    bill pdf

--- protected/views/buyMaterialInput/_formPDF.php
+++ protected/views/buyMaterialInput/_formPDF.php
@@ -6,13 +6,25 @@ require_once('/../tcpdf/tcpdf.php');
 
 class MYPDF extends TCPDF {
 
+	private $site_name;
+	private $site_phone;
+	private $site_address;
+	
+	public function setHeaderInfo($site_name,$site_phone,$site_address) {
+		$this->site_name = $site_name;
+		$this->site_phone = $site_phone;
+		$this->site_address = $site_address;
+		        
+	}
+
     //Page header
     public function Header() {
         
         // Set font
-        //$this->SetFont('helvetica', 'B', 20);
-        // Title
-        //$this->Cell(0, 15, '<< TCPDF Example 003 >>', 0, false, 'C', 0, '', 0, false, 'M', 'M');
+        $this->SetFont('thsarabun', '', 18);
+        $this->writeHTMLCell(145, 20, 0, 2, '<p style="font-weight:bold;">'.$this->site_name.'</p>', 0, 1, false, true, 'C', false);
+        $this->writeHTMLCell(145, 20, 2, 10, '<span style="text-align:center;font-size:14px;font-weight:bold;">ที่อยู่ '.$this->site_address.'</span>', 0, 1, false, true, 'C', false);
+        $this->writeHTMLCell(145, 20, 2, 14, '<span style="text-align:center;font-size:14px;font-weight:bold;">เบอร์โทร '.$this->site_phone.'</span>', 0, 1, false, true, 'C', false);
     }
 
     // Page footer
@@ -28,14 +40,14 @@ class MYPDF extends TCPDF {
         //$this->Image($image_file, 170, 270, 25, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
         $this->Cell(0, 5, date("d/m/Y"), 0, false, 'R', 0, '', 0, false, 'T', 'M');
 
-        $this->writeHTMLCell(145, 550, 70, 200, '-'.$this->getAliasNumPage().'/'.$this->getAliasNbPages().'-', 0, 1, false, true, 'C', false);
+        
         //writeHTMLCell ($w, $h, $x, $y, $html='', $border=0, $ln=0, $fill=false, $reseth=true, $align='', $autopadding=true)
     }
 }
 
 // create new PDF document
 //$pdf = new TCPDF('L', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-$pdf = new MYPDF('L', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+$pdf = new MYPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
@@ -46,7 +58,9 @@ $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
 // set default header data
 //$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
-$pdf->setPrintHeader(false);
+$pdf->setPrintHeader(true);
+$site = Site::model()->FindByPk($model->site_id);
+$pdf->setHeaderInfo($site->name,$site->telephone,$site->address);
 $pdf->setFooterData(array(0,64,0), array(0,64,128));
 
 // set header and footer fonts
@@ -94,12 +108,34 @@ $pdf->SetFont('thsarabun', '', 14, '', true);
 // Set some content to print
 $html = "";
 
-    $html .= '<div style="text-align:center">ข้อมูลด้านการให้บริการ วันที่ (วงเงินไม่รวมภาษีมูลค่าเพิ่ม)</div>';
-    $html .= "<br>";
+$html .= '<br><br><div style="font-weight:bold;font-size:28px;text-align:center"><u>ใบรับซื้อสินค้า</u></div>';
+$html .= '<table border=0><tr style="font-weight:bold;font-size:16px;"><td width="50%">เลขที่ : '.$model->bill_no.'</td>';
+$html .= '<td width="50%" style="text-align:right">วันที่ : '.$model->buy_date.'</td></tr>';
+$html .= '<tr style="font-weight:bold;font-size:16px;"><td colspan=2>ลูกค้า : '.Customer::model()->findByPk($model->customer_id)->name.'</td></tr></table>';
+
+$html .= '<br><br><table border=1><tr style="font-weight:bold;font-size:16px;">
+			<td width="45%" style="text-align:center;">รายการ</td><td width="15%" style="text-align:center;">จำนวน</td><td width="20%" style="text-align:right;">ราคาต่อหน่วย</td><td width="20%" style="text-align:right;">รวมเงิน</td></tr>';
+$details = BuyMaterialDetail::model()->findAll('buy_id='.$model->id);
+$total = 0;
+foreach ($details as $key => $value) {
+	$html .=  '<tr>';
+		$html .=  '<td>'.Material::model()->findByPk($value->material_id)->name.'</td>';
+		$html .=  '<td style="text-align:center;">'.$value->amount.'</td>';
+		$html .=  '<td style="text-align:right;">'.number_format($value->price_unit,2).'</td>';
+		$html .=  '<td style="text-align:right;">'.number_format($value->price_net,2).'</td>';
+	$html .=  '</tr>';	
+
+	$total += $value->price_net;
+}
+$html .=  '<tr style="font-weight:bold;font-size:16px;">';
+		$html .=  '<td colspan=3 width="80%" style="text-align:center;">รวมเป็นเงินทั้งหมด</td>';
+		$html .=  '<td width="20%" style="text-align:right;">'.number_format($total,2).'</td>';
+$html .=  '</tr>';
+$html .= '</table>';
     
   
   
-$pdf->AddPage();
+$pdf->AddPage('P','A5');
 $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
 $pdf->Output($_SERVER['DOCUMENT_ROOT'].'/poontong/report/temp/'.$filename,'F');
 // Print text using writeHTMLCell()
